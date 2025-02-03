ae7e461e2fc29bc14b061d5a293e7e363ad5a86b
GNANESH D GOWDA <gnaneshd12@gmail.com>
2025-02-03 20:20:25 +0530

    Add files via upload

---
+++ t4.js
@@ -0,0 +1,99 @@
+//let num1=10
+
+//num1 +=20
+
+//num1=num1+20
+//=
+//+=
+//-=
+//*=
+// /=
+// %=
+// **=
+
+
+
+
+
+
+
+//let num1 =10
+//let num2 = 20
+
+//let output1= num1+num2
+///let output2= num1-num2
+//let output3= num1*num2
+//let output4= num1/num2
+
+//let output5= 10 % 15
+//let output6 = num1**2
+
+
+
+
+
+
+
+
+//console.log("\n\n\n");
+//console.log('addition operator + ==> output is ',output1);
+//console.log('sub operator - ==> output is ',output2);
+//console.log('mul operator * ==> output is ',output3);
+//console.log('div operator / ==> output is ',output4);
+//console.log('moudulus operator % ==> output is ',output5);
+//console.log('power operator ** ==> output is ',output6);
+//console.log("\n\n\n");
+
+
+
+
+
+
+
+
+//compaeitive operators
+
+
+
+//console.log(x == y)// it compares only values
+//console.log(x === y)//it compares both value and data type
+
+
+//console.log(x != y)
+
+//let  x = "2"
+//let  y = "10"
+
+
+//console.log(x > y);
+
+
+
+
+
+
+
+//logicl operation
+
+
+//isitraining = true
+//isitsunny = false
+
+//console.log(isitraining || isitsunny);
+
+
+
+
+// incriments
+//pre incriment
+//console.log(++num1)
+
+//let num1 = 10
+
+///post incriment
+
+//console.log(num1++)
+//console.log(num1)
+
+
+
