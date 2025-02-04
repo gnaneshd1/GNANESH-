d0fb6bc634f31b85560083070dfde6bd4baa7f82
GNANESH D GOWDA <gnaneshd12@gmail.com>
2025-02-04 20:58:57 +0530

    Add files via upload

---
+++ t5.js
@@ -0,0 +1,95 @@
+//let israining = false
+
+
+//if(israining ){
+
+ //   console.log('yes,its raining today !!!');
+
+//}
+
+//console.log('execution completed!!');
+
+
+
+////let temparature =23
+
+//if(temparature < 20 )
+   // {
+    //    console.log('lets play carrom')
+    
+    
+ //   }
+
+
+//else if(temparature < 22 )
+//{
+ //   console.log('lets play cricket')
+
+
+//}
+//else{
+
+
+   // console.log
+   // ('i dont want to play cricket')
+//
+
+//}
+
+
+
+
+
+
+
+
+
+//
+//let age = 6
+
+///if (age > 17)
+ //    {
+
+
+ //        if (age < 58 )
+ //         {
+  //          console.log('you are citizen');
+  //        }
+
+   //      else{
+    //         console.log('you are sinior citizen');
+   //          }
+
+//
+ //    }
+//
+
+
+//else{
+   
+  //  if (age < 5)
+  //  {
+  //      console.log('stay at home');
+ //   }
+
+ //   else{
+ //       console.log('go to school');
+ //   }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ t6.js
@@ -0,0 +1,99 @@
+//let colors = Array()
+
+//let colors = ['red , green ,blue']
+//let some_array =['india,2.3,455,true,undefined,null,{}']
+
+
+
+
+//let country ="india"
+
+//let country_arr = country.split('')
+
+//country_arr[2]='D'
+//country[2]='D'
+
+//console.log(country_arr[2]);
+//console.log(country[2]);
+
+
+//console.log('Array ==>',country_arr);
+//console.log('string ==>',country);
+
+
+
+
+
+
+//let num_array =Array(3).fill('10')
+
+//num_array[20] = "ahsksh"
+
+//console.log(num_array);
+
+let num1 = [1,3,5]
+let num2 = [10,20,30]
+
+let out = num1.concat(num2)
+
+// console.log('output Array :before  pop ==>',out);
+// out.pop()
+// out.pop()
+// out.pop()
+// out.pop()
+
+// console.log('output Array :After  pop ==>',out);
+
+
+
+
+// console.log('output Array :before  pop ==>',out);
+// out.pop()
+// out.pop()
+// out.pop()
+// out.pop()
+
+// console.log('output Array :After  pop ==>',out);
+
+
+
+
+
+// console.log('output Array :before  shift==>',out);
+// out.shift()
+// out.shift()
+
+
+// console.log('output Array :After shift ==>',out);
+
+
+
+
+
+
+// console.log('output Array :before  unshift==>',out);
+
+// out .unshift(200)
+// out. unshift(22)
+// out. unshift(33)
+
+
+// console.log('output Array :After  unshift ==>',out);
+
+
+
+
+
+// console.log('output array before',out);
+
+// let out2 = out.slice(4,5)
+
+// console.log('output array after slice',out2);
+
+
+
+console.log('before splice ',out);
+
+let out2 = out.splice(2,3)
+
+console.log('before splice',out2);
